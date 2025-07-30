08fcef77b226256e19a0fea63f62e69e172539ce
Ahmed Shammout <ahmed.shammout7@gmail.com>
2025-07-30 10:53:04 -0400

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
-from app.routes import audio
+from .routes import audio, users, recordings, approvals, feedback
+import backend.firebase
 
 app = FastAPI()
+# Initialize Firebase
 app.include_router(audio.router)
+app.include_router(users.router)
+app.include_router(recordings.router)
+app.include_router(approvals.router)
+app.include_router(feedback.router)
+
 
 @app.get("/")
 def read_root():
