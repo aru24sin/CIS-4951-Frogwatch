b59e1fdc38980bb6215a11deb74858ff3a629801
Ahmed Shammout <ashammout7@Ahmeds-MacBook-Air.local>
2025-07-31 22:25:48 -0400

    Update email configuration and route handling in FastAPI app

--- backend/app/email_config.py
+++ backend/app/email_config.py
@@ -1,29 +1,36 @@
 from pydantic_settings import BaseSettings
 from pydantic import EmailStr
 from fastapi_mail import ConnectionConfig
+import os
 
 class Settings(BaseSettings):
-    MAIL_USERNAME: str = "your-email@gmail.com"
-    MAIL_PASSWORD: str = "zmsbuofbfzhaoyjc"
-    MAIL_FROM: EmailStr = "your-email@gmail.com"
-    MAIL_PORT: int = 587
-    MAIL_SERVER: str = "smtp.gmail.com"
-    MAIL_STARTTLS: bool = True
-    MAIL_SSL_TLS: bool = False
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: EmailStr
+    MAIL_FROM_NAME: str = "FrogWatch+"
+    MAIL_PORT: int
+    MAIL_SERVER: str
+    MAIL_STARTTLS: bool
+    MAIL_SSL_TLS: bool
+    USE_CREDENTIALS: bool = True
+    VALIDATE_CERTS: bool
 
     class Config:
-        env_file = ".env"
+        env_file = "backend/.env"
+
 
 settings = Settings()
+print("📧 Loaded EMAIL:", settings.MAIL_USERNAME)
 
 conf = ConnectionConfig(
     MAIL_USERNAME=settings.MAIL_USERNAME,
     MAIL_PASSWORD=settings.MAIL_PASSWORD,
     MAIL_FROM=settings.MAIL_FROM,
+    MAIL_FROM_NAME=settings.MAIL_FROM_NAME,
     MAIL_PORT=settings.MAIL_PORT,
     MAIL_SERVER=settings.MAIL_SERVER,
     MAIL_STARTTLS=settings.MAIL_STARTTLS,
     MAIL_SSL_TLS=settings.MAIL_SSL_TLS,
-    USE_CREDENTIALS=True,
-    VALIDATE_CERTS=True
+    USE_CREDENTIALS=settings.USE_CREDENTIALS,
+    VALIDATE_CERTS=settings.VALIDATE_CERTS
 )
