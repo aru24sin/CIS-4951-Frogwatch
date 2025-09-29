4cc94e898e757de6c7432883d430a23f5d2ee94b
Ahmed Shammout <ahmed.shammout7@gmail.com>
2025-09-28 23:57:43 -0400

    Update backend/.gitignore and set_role_claim script

--- backend/utils/set_role_claim.py
+++ backend/utils/set_role_claim.py
@@ -2,8 +2,8 @@
 import firebase_admin
 from firebase_admin import auth, credentials
 
-# Load your Firebase service account
-cred = credentials.Certificate("backend/firebase-key.json")
+# Load your Firebase service account (ignored by Git)
+cred = credentials.Certificate("backend/keys/frogwatch-service.json")
 firebase_admin.initialize_app(cred)
 
 def set_custom_user_role(uid: str, role: str):
@@ -15,3 +15,4 @@ if __name__ == "__main__":
     # Example: give an admin role to a user
     uid = "HHR5uqZGefYmKOR4aL1uS1OwZPT2"  # replace with real user UID
     set_custom_user_role(uid, "admin")
+
